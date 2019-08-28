ecdacf9f12bf6badd412cc827e9def82bca1e773
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-28 22:51:45 +0000

    Automatically backed up by Learn

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,9 +1,9 @@
-require "spec_helper"
+require_relative "song.rb"
+class Artist
+    attr_accessor :name
 
-describe "Artist" do 
-  it "has a name" do
-    artist = Artist.new
-    artist.name = "Beyonce" 
-    expect(artist.name).to eq("Beyonce")
-  end
+    def initialize
+        @name = "Beyonce"
+    end
+
 end
