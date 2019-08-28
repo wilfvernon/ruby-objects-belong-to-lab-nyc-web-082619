27f4ba45617822a1c0f2ed750bc5fece316ff4f1
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-28 22:58:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 require "artist.rb"
 class Song
   attr_accessor :title, :artist
+  
   def initialize
     @title = '7/11'
-    song.artist = Artist.name
+    self.artist = Artist.name
   end
+  
 end
